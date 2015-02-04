91c1135c9d8dae45d893dfa6cfac3e67f7b3e214
Justin Mikell <justin.mikell@gmail.com>
2015-02-03 20:09:35 -0600

    update so it runs

---
+++ B1/src/VoxelSValuesDetectorConstruction.cc
@@ -0,0 +1,160 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+// $Id$
+//
+/// \file VoxelSValues.cc
+/// \brief Implementation of the VoxelSValues class
+
+#include "VoxelSValuesDetectorConstruction.hh"
+#include "VoxelSValuesSteppingAction.hh"
+   // use of stepping action to set the accounting volume
+
+#include "G4RunManager.hh"
+#include "G4NistManager.hh"
+#include "G4Box.hh"
+#include "G4Cons.hh"
+#include "G4Orb.hh"
+#include "G4Sphere.hh"
+#include "G4Trd.hh"
+#include "G4LogicalVolume.hh"
+#include "G4PVPlacement.hh"
+#include "G4SystemOfUnits.hh"
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+VoxelSValuesDetectorConstruction::VoxelSValuesDetectorConstruction()
+: G4VUserDetectorConstruction()
+{ }
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+VoxelSValuesDetectorConstruction::~VoxelSValuesDetectorConstruction()
+{ }
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+G4VPhysicalVolume* VoxelSValuesDetectorConstruction::Construct()
+{  
+  // Get nist material manager
+  G4NistManager* nist = G4NistManager::Instance();
+  
+  // Phantom parameters
+  //  30 cm water cube
+  G4double phantom_sizeXY = 30*cm, phantom_sizeZ = 30*cm;
+  G4Material* phantom_mat = nist->FindOrBuildMaterial("G4_WATER");
+   
+  // Option to switch on/off checking of volumes overlaps
+  //
+  G4bool checkOverlaps = true;
+
+  //     
+  // World
+  //
+  G4double world_sizeXY = 1.2*phantom_sizeXY;
+  G4double world_sizeZ  = 1.2*phantom_sizeZ;
+  G4Material* world_mat = nist->FindOrBuildMaterial("G4_AIR");
+  
+  G4Box* solidWorld =    
+    new G4Box("World",                       //its name
+       0.5*world_sizeXY, 0.5*world_sizeXY, 0.5*world_sizeZ);     //its size
+      
+  G4LogicalVolume* logicWorld =                         
+    new G4LogicalVolume(solidWorld,          //its solid
+                        world_mat,           //its material
+                        "World");            //its name
+                                   
+  G4VPhysicalVolume* physWorld = 
+    new G4PVPlacement(0,                     //no rotation
+                      G4ThreeVector(),       //at (0,0,0)
+                      logicWorld,            //its logical volume
+                      "World",               //its name
+                      0,                     //its mother  volume
+                      false,                 //no boolean operation
+                      0,                     //copy number
+                      checkOverlaps);        //overlaps checking
+                     
+  //     
+  // Phantom
+  //  
+  G4Box* solidPhantom =    
+    new G4Box("Phantom",                    //its name
+        0.5*phantom_sizeXY, 0.5*phantom_sizeXY, 0.5*phantom_sizeZ); //its size
+      
+  G4LogicalVolume* logicPhantom =                         
+    new G4LogicalVolume(solidPhantom,            //its solid
+                        phantom_mat,             //its material
+                        "Phantom");         //its name
+               
+  new G4PVPlacement(0,                       //no rotation
+                    G4ThreeVector(),         //at (0,0,0)
+                    logicPhantom,                //its logical volume
+                    "Phantom",              //its name
+                    logicWorld,              //its mother  volume
+                    false,                   //no boolean operation
+                    0,                       //copy number
+                    checkOverlaps);          //overlaps checking
+ 
+  //xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx     
+  // source voxel
+  //   We explicitly create a source voxel for scoring purposes in line with example B1.
+  //
+  
+  G4Material* sourceVoxelMat = nist->FindOrBuildMaterial("G4_WATER");
+  G4ThreeVector pos1 = G4ThreeVector(0*cm, 0*cm, 0*cm);
+        
+  // Box shape for isotropic voxel
+  G4double dx=3*mm;
+  G4Box* solidVoxel = 
+    new G4Box( "solidVoxel", 0.5*dx, 0.5*dx, 0.5*dx);
+
+  G4LogicalVolume* logicVoxel =                         
+    new G4LogicalVolume(solidVoxel,         //its solid
+                        sourceVoxelMat,          //its material
+                        "solidVoxel");           //its name
+               
+  new G4PVPlacement(0,                       //no rotation
+                    pos1,                    //at position
+                    logicVoxel,             //its logical volume
+                    "solidVoxel",                //its name
+                    logicPhantom,            //its mother  volume
+                    false,                   //no boolean operation
+                    0,                       //copy number
+                    checkOverlaps);          //overlaps checking
+                
+  // Set scoring volume to stepping action 
+  // (where we will account energy deposit)
+  //
+  VoxelSValuesSteppingAction* steppingAction = VoxelSValuesSteppingAction::Instance(); 
+  ////steppingAction->SetVolume(logicShape1);
+  steppingAction->SetVolume(logicVoxel);
+
+  //
+  //always return the physical World
+  //
+  return physWorld;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
---
+++ B1/src/VoxelSValuesPrimaryGeneratorAction.cc
@@ -0,0 +1,137 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+//  Modified by Justin Mikell for isotropic spatial and directional sampling
+//  inside the source voxel. 
+//
+// $Id$
+//
+/// \file VoxelSValuesPrimaryGeneratorAction.cc
+/// \brief Implementation of the VoxelSValuesPrimaryGeneratorAction class
+
+#include "VoxelSValuesPrimaryGeneratorAction.hh"
+
+#include "G4LogicalVolumeStore.hh"
+#include "G4LogicalVolume.hh"
+#include "G4Box.hh"
+#include "G4RunManager.hh"
+#include "G4ParticleGun.hh"
+#include "G4ParticleTable.hh"
+#include "G4ParticleDefinition.hh"
+#include "G4SystemOfUnits.hh"
+#include "Randomize.hh"
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+VoxelSValuesPrimaryGeneratorAction* VoxelSValuesPrimaryGeneratorAction::fgInstance = 0;
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+const VoxelSValuesPrimaryGeneratorAction* VoxelSValuesPrimaryGeneratorAction::Instance()
+{
+// Static acces function via G4RunManager 
+
+  return fgInstance;
+}      
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+VoxelSValuesPrimaryGeneratorAction::VoxelSValuesPrimaryGeneratorAction()
+: G4VUserPrimaryGeneratorAction(),
+  fParticleGun(0)
+{
+  G4int n_particle = 1;
+  fParticleGun  = new G4ParticleGun(n_particle);
+
+  // default particle kinematic
+  G4ParticleTable* particleTable = G4ParticleTable::GetParticleTable();
+  G4String particleName;
+  G4ParticleDefinition* particle
+    = particleTable->FindParticle(particleName="gamma");
+  fParticleGun->SetParticleDefinition(particle);
+  fParticleGun->SetParticleMomentumDirection(G4ThreeVector(0.,0.,1.));
+  fParticleGun->SetParticleEnergy(1.*MeV);
+  
+  fgInstance = this;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+VoxelSValuesPrimaryGeneratorAction::~VoxelSValuesPrimaryGeneratorAction()
+{
+  delete fParticleGun;
+  fgInstance = 0;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void VoxelSValuesPrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent)
+{
+  //this function is called at the beginning of each event
+  //
+
+  // In order to avoid dependence of PrimaryGeneratorAction
+  // on DetectorConstruction class we get Phantom volume
+  // from G4LogicalVolumeStore.
+  
+
+  G4double dXYZ = 0;
+  G4LogicalVolume* voxelLV
+    = G4LogicalVolumeStore::GetInstance()->GetVolume("solidVoxel");
+  G4Box* voxelBox = NULL;
+  if ( voxelLV ) voxelBox = dynamic_cast<G4Box*>(voxelLV->GetSolid());
+  if ( voxelBox ) {
+    dXYZ = voxelBox->GetXHalfLength()*2.;
+  }  
+  else  {
+    G4cerr << "source voxel shape not found." << G4endl;
+    G4cerr << "Perhaps you have changed geometry." << G4endl;
+    G4cerr << "The gun will be place in the center." << G4endl;
+  }
+
+  //add energy sampling here...
+
+  //sample position within the source voxel
+  G4double x0 = dXYZ * (G4UniformRand()-0.5);
+  G4double y0 = dXYZ * (G4UniformRand()-0.5);
+  G4double z0 = dXYZ * (G4UniformRand()-0.5);
+  
+  //sample isotropic direction 
+  G4double cosTheta = -1.0 + 2.0*G4UniformRand();
+  G4double phi = CLHEP::twopi*G4UniformRand();
+  G4double sinTheta = sqrt(1. - cosTheta*cosTheta);
+  
+  G4double px0 = sinTheta*cos(phi);
+  G4double py0 = sinTheta*sin(phi);
+  G4double pz0 = cosTheta;
+
+  fParticleGun->SetParticlePosition(G4ThreeVector(x0,y0,z0));
+  fParticleGun->SetParticleMomentumDirection(G4ThreeVector(px0,py0,pz0));
+
+  fParticleGun->GeneratePrimaryVertex(anEvent);
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
