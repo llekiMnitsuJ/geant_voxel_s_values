a775c3a9e7c57a7c94624d96ea2a94c379d8cc2d
Justin Mikell <justin.mikell@gmail.com>
2015-02-05 20:19:11 -0600

    Switching to G4GeneralParticleSource, so I can read in a histogram.

--- source_voxel_absorbed_dose/include/PrimaryGeneratorAction.hh
+++ source_voxel_absorbed_dose/include/PrimaryGeneratorAction.hh
@@ -32,10 +32,9 @@
 #define VoxelSValuesPrimaryGeneratorAction_h 1
 
 #include "G4VUserPrimaryGeneratorAction.hh"
-#include "G4ParticleGun.hh"
 #include "globals.hh"
 
-class G4ParticleGun;
+class G4GeneralParticleSource;
 class G4Event;
 class VoxelSValuesDetectorConstruction;
 
@@ -57,12 +56,12 @@ class VoxelSValuesPrimaryGeneratorAction : public G4VUserPrimaryGeneratorAction
     virtual void GeneratePrimaries(G4Event*);         
   
     // method to access particle gun
-    const G4ParticleGun* GetParticleGun() const { return fParticleGun; }
+    const G4GeneralParticleSource* GetGeneralParticleSourceGun() const { return fParticleGun; }
   
   private:
     static VoxelSValuesPrimaryGeneratorAction* fgInstance;
    
-    G4ParticleGun*  fParticleGun; // pointer a to G4 gun class
+    G4GeneralParticleSource*  fParticleGun; // pointer a to G4 gun class
 };
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
